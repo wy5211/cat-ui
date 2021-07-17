01fc4324a43bb0b48c45c25b061b2fa38b0a9897
wy5211 <1579151657@qq.com>
2021-07-17 20:49:06 +0800

    完成Button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <div>app</div>;
+import React from 'react';
+import Button from './components/button';
+
+export default function App(): React.ReactElement {
+  return (
+    <div style={{ margin: 20 }}>
+      <h2>Button </h2>
+      <Button
+        size="lg"
+        onClick={() => {
+          alert('点击了');
+        }}
+      >
+        primary lg
+      </Button>
+
+      <Button btnType="primary" disabled>
+        default
+      </Button>
+      <Button btnType="danger">primary</Button>
+      <Button btnType="link">link</Button>
+    </div>
+  );
 }
